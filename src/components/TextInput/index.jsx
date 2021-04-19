c4ef9432e4df1e64358682d60f30fdf88549f5bd
Guilherme Alvarez <gui06barboza@gmail.com>
2021-04-19 01:44:22 -0300

    input search

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,1 +1,9 @@
 import './styles.css';
+
+export const TextInput = ({type, value, placeholder, onChange}) => {
+    return(
+        <>
+            <input type={type} value={value} onChange={onChange} placeholder={placeholder} />
+        </>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Post } from '../../components/Post';
+import { TextInput } from '../../components/TextInput';
 import { loadPost } from '../../utils/loadPost';
 import './styles.css';
 
 function App() {
   const [post, setPost] = useState([]);
+  const [search, setSearch] = useState('');
 
   const handleLoadPost = useCallback(async () => {
     const load = await loadPost();
@@ -12,13 +14,27 @@ function App() {
     setPost(load);
   }, []);
 
+  const handleChange = ({target}) => {
+    const {value} = target;
+
+    setSearch(value);
+  }
+
+  const filteredposts = !!search ? post.filter(post => {
+    return post.title.toLowerCase().includes(search.toLocaleLowerCase());
+  }) : post;
+
   useEffect(() => {
     handleLoadPost();
   }, [handleLoadPost])
 
   return (
     <div className="App">
-      <Post posts={post} />
+      <div className="textinput-container">
+        <TextInput type="search" value={search} onChange={handleChange}/>
+      </div>
+      {filteredposts.length > 0 && <Post posts={filteredposts} />}
+      {filteredposts.length === 0 && <h2>No posts :(</h2>}
     </div>
   );
 }
